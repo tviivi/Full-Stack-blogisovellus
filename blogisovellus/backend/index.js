93f8ad8c35a11ad36f05fe0040f99a8a472f90c2
tviivi <viivi.tiihonen@helsinki.fi>
2018-06-11 12:45:16 +0300

    frontti herokuun

--- blogisovellus/backend/index.js
+++ blogisovellus/backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 
 let blogs = [
--- blogisovellus/frontend/src/services/blogs.js
+++ blogisovellus/frontend/src/services/blogs.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/blogs'
+const baseUrl = '/blogs'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
